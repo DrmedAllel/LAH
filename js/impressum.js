9e25c51b133864e3bb9ee417dab8140268f97e12
DrmedAllel <claudius.caspar.laur@gmail.com>
2025-02-06 14:23:49 +0100

    Korrigiere die Adresse im Impressum und verbessere die Struktur der Links

--- js/impressum.js
+++ js/impressum.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
             <h2>${language === 'de' ? 'Verantwortlich für den Inhalt gemäß § 10 Absatz 3 MDStV' : 'Responsible for content according to § 10 paragraph 3 MDStV'}</h2>
             
             <p>Udo Hafner<br>
-            c/o Luftfahrt-Archiv Hafner<br>
+            Luftfahrt-Archiv Hafner<br>
             Salonallee 5<br>
             D-71638 Ludwigsburg</p>
     
